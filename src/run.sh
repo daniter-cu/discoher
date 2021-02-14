623f8a0b1f8aa196bb2c2dfe2894afaebdd68bc3
Dan Iter <iter.dan@gmail.com>
2021-02-13 19:25:54 -0800

    refresh runner

--- src/run.sh
+++ src/run.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
 python training.py --contrasts=16 --emsize 768 --nhid 1536 --bptt 256 --batch_size 16 \
---nlayers 2 --log-interval 50 --lr 1 --cuda --save_interval 100
+--nlayers 2 --log-interval 50 --lr 1 --cuda --save_interval 100 \
+--restore ../checkpoints/run1/chkpt-1-2000-model.pt
