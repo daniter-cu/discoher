434d6c24ff68e85bc487c91378d04ef8a5a4941d
Dan Iter <iter.dan@gmail.com>
2021-02-18 14:29:21 -0800

    update run script

--- src/run.sh
+++ src/run.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 python training.py --contrasts=16 --emsize 768 --nhid 1536 --bptt 256 --batch_size 16 \
---nlayers 2 --log-interval 50 --lr 1 --cuda --save_interval 100 \
---restore ../checkpoints/run1/chkpt-1-2000-model.pt
+--nlayers 2 --log-interval 200 --lr 1 --cuda --save_interval 500 \
+--restore ../checkpoints/run1/chkpt-1-3000-model.pt
