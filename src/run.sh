583a33bcdbeb1d9fddf1984f8f63f08f3705ad93
Dan Iter <iter.dan@gmail.com>
2021-03-01 11:36:09 -0800

    fixed run]

--- src/run.sh
+++ src/run.sh
@@ -2,4 +2,4 @@
 
 python training.py --contrasts=16 --emsize 768 --nhid 1536 --bptt 256 --batch_size 16 \
 --nlayers 2 --log-interval 200 --lr 1 --cuda --save_interval 500 \
---restore ../checkpoints/run1/chkpt-1-3000-model.pt
+--restore ../checkpoints/chkpt-1-3000-model.pt
